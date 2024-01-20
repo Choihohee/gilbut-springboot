9eae6a44060203f4520da043feccb80e8e2bc343
Choihohee <lovelove4927@naver.com>
2024-01-20 18:56:08 +0900

    first commit

---
+++ src/main/java/com/gilbut/gilbut/GilbutApplication.java
@@ -0,0 +1,13 @@
+package com.gilbut.gilbut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilbutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilbut/gilbut/GilbutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilbut.gilbut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
